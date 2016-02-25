326fb3e753f144494d339f3a5b9e35b9675f7940
somethingsaid <kwong.billy@gmail.com>
2016-02-25 15:44:01 -0500

    Added sick theme song

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+var audiocue = 0;
 // define Audio for hadouken
 function playHadouken () {
     $('#hadouken-sound')[0].volume = 0.5;
@@ -12,9 +13,14 @@ function playTheme () {
 	$('#ryu-theme')[0].load();
 	$('#ryu-theme')[0].loop = true;
 	$('#ryu-theme')[0].play();
+	audiocue = 1;
 }
 
-  playTheme();
+function stopTheme() {
+	$('#ryu-theme')[0].pause();
+	$('#ryu-theme')[0].currenttime = 0
+	audiocue = 0;
+}
 // method chaining - browser listens for a series of events on .ryu div
 $(".ryu").mouseenter(function() {
 	// hide all other states, show only .ryu-ready
@@ -60,6 +66,21 @@ $(document).keydown(function(e) {
         $(".ryu-still").show();
     }
 });
+
+// play theme song when pressing 'p' key
+$(document).keypress(function(e) {
+	if (e.keyCode == 112) {
+        if (audiocue == 0) {
+		    playTheme();
+		    console.log("starting music");
+	    }
+	    else {
+	    	stopTheme();
+	    	console.log("stopping music");
+	    }
+	}
+})
+;
 
 //End of file
 });
