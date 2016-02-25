2a6f0120c7d2db6b29de3ac16bc3721e87c8810f
somethingsaid <kwong.billy@gmail.com>
2016-02-25 14:29:06 -0500

    Almost there

--- js/app.js
+++ js/app.js
@@ -1,61 +1,63 @@
 $(document).ready(function() {
-	// method chaining - browser listens for a series of events on .ryu div
-    $(".ryu").mouseenter(function() {
-    	$('.ryu-still').hide();
-        $('.ryu-ready').show();
-    })
-    .mouseleave(function() {
-    	$('.ryu-ready').hide();
-    	$('.ryu-still').show();
-    })
-    .mousedown(function() {
-    	playHadouken();
-    	$('.ryu-ready').hide();
-    	$('.ryu-throwing').show();
-    	$('.hadouken').finish().show().animate(
-    		// .finish() ensures animation reaches end-state (solves bug of hadouken not starting from hands when multiple mouse clicks)
-    		// desired property: i.e. end-state is want css to position hadouken at left: 1060px
-    		// 500 indicates duration taken to reach end-state
-    		// finally, call another function to hide current element, reset position
-    		{'left': '1060px'}, 500, function() {
-    			$(this).hide();
-                $(this).css('left', '560px');
-            }
-);
-    })
-    .mouseup(function() {
 
-    	$('.ryu-throwing').hide();
-    	$('.ryu-ready').hide();
-    	$('.ryu-still').show()
-    });
-    
-    // solving bug - when mouseup over .hadouken, both .ryu-ready and .ryu-throwing are displayed
-    $(".hadouken").mouseup(function() {
-    	$('.ryu-throwing').hide();
-    });
+// define Audio for hadouken
+function playHadouken () {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
 
-    // make Ryu look cool when pressing 'x' key
-    $(document).keydown(function(e) {
-    	if (e.keyCode == 88) {
-    		$(".ryu-ready").hide();
-    		$(".ryu-still").hide()
-    		$(".ryu-cool").show();
-    	}
-    })
-    .keyup(function(e) {
-    	if (e.keyCode == 88) {
-    	    $(".ryu-cool").hide();
-    	    $(".ryu-still").show();
-    	}
-    });
+// method chaining - browser listens for a series of events on .ryu div
+$(".ryu").mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+})
+.mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+})
+.mousedown(function() {
+    playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-still').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show().animate(
+	    // .finish() ensures animation reaches end-state (solves bug of hadouken not starting from hands when multiple mouse clicks)
+	    // desired property: i.e. end-state is want css to position hadouken at left: 1060px
+	    // 500 indicates duration taken to reach end-state
+	    // finally, call another function to hide current element, reset position
+	    {'left': '1060px'}, 500, function() {
+		    $(this).hide();
+            $(this).css('left', '560px');
+        }
+    );
+})
+.mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').hide();
+    $('.ryu-still').show()
+});
 
-    // define Audio for hadouken
-    function playHadouken () {
-    	$('#hadouken-sound')[0].volume = 0.5;
-        $('#hadouken-sound')[0].load();
-        $('#hadouken-sound')[0].play();
+// solving bug - when mouseup over .hadouken, both .ryu-ready and .ryu-throwing are displayed
+$(".hadouken").mouseup(function() {
+    $('.ryu-throwing').hide();
+});
+
+// make Ryu look cool when pressing 'x' key
+$(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+	    $(".ryu-ready").hide();
+	    $(".ryu-still").hide()
+	    $(".ryu-cool").show();
     }
+})
+.keyup(function(e) {
+    if (e.keyCode == 88) {
+        $(".ryu-cool").hide();
+        $(".ryu-still").show();
+    }
+});
+
 //End of file
 });
 
