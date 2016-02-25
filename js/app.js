2693e27681e205cacd5dcea59053e9dec7d620e5
somethingsaid <kwong.billy@gmail.com>
2016-02-25 13:34:00 -0500

    Added make Ryu look cool when pressing x

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	//method chaining - browser listens for a series of events on .ryu div
+	// method chaining - browser listens for a series of events on .ryu div
     $(".ryu").mouseenter(function() {
     	$('.ryu-still').hide();
         $('.ryu-ready').show();
@@ -13,10 +13,10 @@ $(document).ready(function() {
     	$('.ryu-ready').hide();
     	$('.ryu-throwing').show();
     	$('.hadouken').finish().show().animate(
-    		//.finish() ensures animation reaches end-state (solves bug of hadouken not starting from hands when multiple mouse clicks)
-    		//desired property: i.e. end-state is want css to position hadouken at left: 1060px
+    		// .finish() ensures animation reaches end-state (solves bug of hadouken not starting from hands when multiple mouse clicks)
+    		// desired property: i.e. end-state is want css to position hadouken at left: 1060px
     		// 500 indicates duration taken to reach end-state
-    		//finally, call another function to hide current element, reset position
+    		// finally, call another function to hide current element, reset position
     		{'left': '1060px'}, 500, function() {
     			$(this).hide();
                 $(this).css('left', '560px');
@@ -29,12 +29,22 @@ $(document).ready(function() {
     	$('.ryu-ready').show();
     });
 
-    //define Audio for hadouken
+    // make Ryu look cool when pressing 'x' key
+    $(document).keydown(function(e) {
+    	if (e.which == 88) {
+    		$(".ryu-ready").hide();
+    		$(".ryu-throwing").hide();
+    		$(".ryu-still").hide();
+    		$(".ryu-cool").show();
+    	}
+    });
+
+    // define Audio for hadouken
     function playHadouken () {
     	$('#hadouken-sound')[0].volume = 0.5;
         $('#hadouken-sound')[0].load();
         $('#hadouken-sound')[0].play();
     }
-//End of functions
+//End of file
 });
 
